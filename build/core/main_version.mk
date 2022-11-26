88c546c82173722def924b4a379109cf3cffa823
aghora7 <ukikieran@gmail.com>
2022-11-26 18:57:54 +0530

    vendor : Init lessaosp tigris
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -2,7 +2,7 @@
 ifeq ($(BUILD_FINGERPRINT),)
 BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
 CUSTOM_DEVICE ?= $(TARGET_DEVICE)
-ifneq ($(filter OFFICIAL,$(CUSTOM_BUILD_TYPE)),)
+ifneq ($(filter OFFICIAL,$(LESSAOSP_BUILD_TYPE)),)
 BUILD_SIGNATURE_KEYS := release-keys
 else
 BUILD_SIGNATURE_KEYS := test-keys
@@ -23,9 +23,11 @@ CUSTOM_SECURITY_PATCH := 2022-08-05
 
 # Versioning props
 ADDITIONAL_SYSTEM_PROPERTIES  += \
-    org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
-    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE) \
-    org.pixelexperience.build_security_patch=$(CUSTOM_SECURITY_PATCH)
+    org.lessaosp.version=$(LESSAOSP_VERSION_PROP) \
+    org.lessaosp.version.display=$(CUSTOM_VERSION) \
+    org.lessaosp.build_date=$(CUSTOM_BUILD_DATE) \
+    org.lessaosp.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    org.lessaosp.build_type=$(LESSAOSP_BUILD_TYPE) \
+    org.lessaosp.build_security_patch=$(CUSTOM_SECURITY_PATCH) \
+    org.lessaosp.codename=$(LESSAOSP_CODENAME) \
+    ro.lessaosp.maintainer=$(LESSAOSP_MAINTAINER)
